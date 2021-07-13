036ee73c76222f05419f3b0a3fdb030c88a2b63d
phamchinghi11@gmail.com <phamchinghi11@gmail.com>
2021-07-13 16:01:13 +0700

    Done xóa 1 sản phẩm bằng Ajax

---
+++ OnlineShop/Areas/Admin/Models/Dao/ProductDao.cs
@@ -0,0 +1,32 @@
+﻿using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Areas.Admin.Models.Dao
+{
+    public class ProductDao
+    {
+        ShopDbContext db = null;
+
+        public ProductDao()
+        {
+            db = new ShopDbContext();
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                var product = db.Products.Find(id);
+                db.Products.Remove(product);
+                db.SaveChanges();
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+            
+        }
+    }
+}
